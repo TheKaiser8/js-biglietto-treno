56d4b3718f3f5067f2ae31277fce470deac9fd67
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-10-20 15:17:30 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// 1. Chiedi all'utente il numero di km che vuole percorrere e salva il dato
+// 2. Chiedi all'utente l'età del passeggero e salva il dato
+// 3. Definisci la costante del prezzo al km (€/km)
+// 4. Determina il prezzo del biglietto
+//    4a. SE il passeggero è minorenne va applicato uno sconto del 20%
+//        ALTRIMENTI SE il passeggero ha un'età maggiore o uguale a 65 anni va applicato uno sconto del 40%
+//        ALTRIMENTI il prezzo del biglietto sarà intero senza sconti
+//    4b. Trovare il modo per indicare il prezzo con massimo due cifre decimali
+// 5. Stampa sulla pagina HTML il prezzo del biglietto del treno
